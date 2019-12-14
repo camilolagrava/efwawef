923f4a9770ba22b2cf0d0149b66238e6d4e302a6
unknown <camilolagrava@gmail.com>
2019-12-14 08:42:09 -0400

    ...

--- server/database.js
+++ server/database.js
@@ -2,6 +2,9 @@ const mongoose = require('mongoose');
 
 const URI = 'mongodb://localhost/sistema-inventario';
 
+//conectividad a la base de datos
+
+
 mongoose.connect(URI)
       .then(db => console.log('DB is conected'))
       .catch(err => console.error(err));
